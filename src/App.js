8a5d082d99ca3e62b671bb6be57c75ea47a75d46
FernandoAgostini28 <fernando.agostini.28@gmail.com>
2022-03-13 19:30:42 -0300

    navbar e tela de cadastro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import FromFornecedor from './components/Form';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import ResponsiveAppBar from './components/navbar/Navbar'
+import FromFornecedor from './components/fornecedor/Cadastrar';
 import './App.css';
+import Listar from "./components/fornecedor/Editar";
 
 function App() {
   return (
-    <div className="App">
-      <FromFornecedor />
-    </div>
+    
+    <BrowserRouter>
+    <ResponsiveAppBar />
+      <Routes>
+        <Route path="/" element={<FromFornecedor />} />
+        <Route path="Cadastrar" element={<FromFornecedor />} />
+        <Route path="Listar" element={<Listar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/fornecedor/Editar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Listar(){
+    return(
+        <h1>Ola mundo</h1>
+    )
+}
